459d8f6b7af27b61792c9fbb13ad1236592c8a60
ToriLutz <vglutz@comcast.net>
2024-10-11 15:26:24 -0700

    Update wordpuzzle.py

--- wordpuzzle.py
+++ wordpuzzle.py
@@ -37,12 +37,14 @@ for attempt in range(1, 7):
     guess_count += 1
     if guess == game_word:
       print(f"Correct, the game word was {game_word}. You guessed it in {guess_count} tries!")
+      quit()
     elif len(guess) != len(game_word):
       print(f"You've entered the wrong length of word. Please enter a {len(game_word)} letter word: ")
     elif guess_count > 6:
       print(f"You ran out of tries! Sorry, your word was {game_word}")
+      quit()
     else:
-        for i in range(min(len(guess))):
+        for i in range(len(guess)):
             if guess[i] == game_word[i]:
             
                 print(guess[i].upper(), end=' ')
