8acf648c6230e735b27eaaa5722d0b96e9e52344
ToriLutz <vglutz@comcast.net>
2024-10-25 07:57:00 -0700

    Update data.py

--- data.py
+++ data.py
@@ -4,29 +4,32 @@
 import pathlib
 from pathlib import Path
 
-maximum = 0
-minimum = 100
 minimum_country = ''
 maximum_country = ''
-
 input_country_max = ''
 input_country_min = ''
 min_year = ''
 max_year = ''
+
+country_year_small = 100
+country_year_big = 0
+country_data_min = 100
+country_data_max = 0
+maximum = 0
+minimum = 100
 year_maximum = 0
-# TODO from dad. You want this to start high so it will decrease
 year_minimum = 100
 year = 0
 year_sum = 0
 year_count = 0
 year_average = 0
 
-
-file_path = Path('../life-expectancy.csv')
+file_path = Path('life-expectancy.csv')
 text = file_path.read_text()
 lines = text.splitlines()
 
 year_input = int(input('Please enter the year of interest: '))
+country_input = input('Please enter a country: ')
 
 
 for index, line in enumerate(lines):
@@ -50,35 +53,48 @@ for index, line in enumerate(lines):
         if year_input == year:
             year_sum += life_expectancy
             year_count += 1
-            # TODO from dad. I moved the if statements up here. Down below it is outside
-            # the loop. The if statement is on the same column as the for loop that's why it
-            # only hit zimbabwe
+           
             if life_expectancy < year_minimum:
                 year_minimum = life_expectancy
                 input_country_min = country
             if life_expectancy > year_maximum:
                 year_maximum = life_expectancy
                 input_country_max = country
+                
+        if country_input == country:
+          country_input = country
+          if life_expectancy < country_data_min:
+            country_data_min = life_expectancy
+            country_year_small= year
+          if life_expectancy > country_data_max:
+            country_data_max = life_expectancy
+            country_year_big = year
+                     
 
-# TODO from dad. you removed two print statements here that showed the overall min and max
-# I copied these back over from your original file.
 print(
-    f'The max life expectancy was in {maximum_country} with {maximum} in the year {max_year}')
+    f'The overall max life expectancy was in {maximum_country} with {maximum} in the year {max_year}')
 print(
-    f'The minimum life expectancy was in {minimum_country} with {minimum} in the year {min_year}')
+    f'The overall minimum life expectancy was in {minimum_country} with {minimum} in the year {min_year}')
+
+
+if country_input == country:
+  print(f'For the country {country_input}')
+  print(
+    f'The max life expectancy in {country_input} was in {country_year_big} with {country_data_max}\n')
+  print(f'The min life expectancy in {country_input} was in {country_year_small} with {country_data_min}\n ')
+
+
 
 if year_count > 0:
     year_average = year_sum / year_count
 
-    print(f"For the year {year_input}:")
-    # TODO from dad. You might want to do some formatting here, like indent everything after
-    # the year with a \t in the print statement to group the year specific data. maybe add some \n
-    # to give some space to make it more readable
+    print(f'For the year {year_input}:')
+
     print(
-        f"The average life expectancy across all countries was {year_average}")
+        f'The average life expectancy across all countries was {year_average}\n')
     print(
-        f'"The max life expectancy was in {input_country_max} with {year_maximum}"')
+        f'The max life expectancy was in {input_country_max} with {year_maximum}\n')
     print(
-        f'The min life expectancy was in {input_country_min} with {year_minimum}')
+        f'The min life expectancy was in {input_country_min} with {year_minimum}\n')
 else:
     print("There is no data available for the year you entered. ")
