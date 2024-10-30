44b3bfe477afbea4cbe536bb84dd912a9287edef
Brett Lutz <brett.lutz@dynatacorp.com>
2024-10-29 21:15:47 -0600

    removed unused print statements and added degree symbol in output

--- wind_chill.py
+++ wind_chill.py
@@ -1,24 +1,22 @@
 def celsius_to_fahrenheit(temp):
-    return (float(temp) - 32) * 5/9
+    return (temp - 32) * 5/9
 
 
 def fahrenheit_to_celsius(temp):
-    return (9 / 5) * float(temp) + 32
+    return (9 / 5) * temp + 32
 
 
 def calculate_windchill(temp, type):
-    print(f"Passed in temp: {temp}")
-    print(f"Passed in type: {type}")
-    beginning_temp = float(temp)
+    beginning_temp = temp
     if type.lower() == 'c':
         beginning_temp = celsius_to_fahrenheit(temp)
     for speed in list(range(5, 65, 5)):
-        chill = 35.74 + 0.6215 * float(beginning_temp) - 35.75 * \
-            speed**0.16 + 0.4275 * float(beginning_temp) * speed**0.16
+        chill = 35.74 + 0.6215 * beginning_temp - 35.75 * \
+            speed**0.16 + 0.4275 * beginning_temp * speed**0.16
         print(
-            f"At temperature {'{: .2f}'.format(beginning_temp)}F, and wind speed of {speed} mph, the windchill is: {'{: .2f}'.format(chill)}F")
+            f"At temperature {'{: .2f}'.format(beginning_temp)}°F, and wind speed of {speed} mph, the windchill is: {'{: .2f}'.format(chill)}°F")
 
 
-temperature = input("What is the temperature? ")
+temperature = float(input("What is the temperature? "))
 celsius_fahrenheit = input("Fahrenheit or Celsius (F/C)? ")
 calculate_windchill(temperature, celsius_fahrenheit)
